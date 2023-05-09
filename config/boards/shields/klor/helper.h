0e6e4ccac2c8fe32558c84fedd31701dccf932d7
MajykOyster <yoann.yvin@etik.com>
2023-05-10 01:06:27 +0200

    Update helper.h

--- config/boards/shields/klor/helper.h
+++ config/boards/shields/klor/helper.h
@@ -131,7 +131,6 @@
                 tap-ms = <0>; \
                 #binding-cells = <0>; \
                 bindings = <OS_UNICODE_LEAD>, <&macro_tap unicode_bindings>, <OS_UNICODE_TRAIL>; \
-/*                bindings = <OS_UNICODE_LEAD>, <&macro_tap unicode_bindings>, <OS_UNICODE_TRAIL>; \ */
             }; \
         }; \
     };
