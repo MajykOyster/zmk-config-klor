95ec637cd3eaa73d5648fd89137c3bfd61b85806
MajykOyster <yoann.yvin@etik.com>
2023-05-11 11:05:42 +0200

    Update helper.h

--- config/boards/shields/klor/helper.h
+++ config/boards/shields/klor/helper.h
@@ -107,8 +107,7 @@
     #elif HOST_OS == 1
         #define OS_UNICODE_LEAD &macro_tap &kp LS(LC(U))   // Linux
     #else
-        #define OS_UNICODE_LEAD &macro_tap &kp RALT  // Windows + WinCompose (default)
-    #endif
+        #define OS_UNICODE_LEAD &macro_tap &kp RALT &kp U  // Windows + WinCompose (default)
 #endif
 #if !defined OS_UNICODE_TRAIL
     #if HOST_OS == 2
